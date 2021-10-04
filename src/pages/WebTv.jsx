6be38d5a127ea40d81de13c73aa049f12c5e6ba0
Kameli71 <karim.kameli71@gmail.com>
2021-10-04 19:55:43 +0200

    front modif

--- src/pages/WebTv.jsx
+++ src/pages/WebTv.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import ReactTwitchEmbedVideo from "react-twitch-embed-video";
+import Image from "../assets/Twitch.png";
 import './WebTv.css';
 
 function WebTv() {
     return (
         <div>
-            
+            <img id="logo" src={Image} alt="Logo" />
+            <div className="webtv-title">
+            <h1>NOTRE WEB TV</h1>
+            </div>
+            <div className="twitch-container">
+            <ReactTwitchEmbedVideo channel="talk2megooseman" />
+            </div>
         </div>
     )
 }
